35262c324a2735cc548757b9b5e2728f2f33e12b
Tranduy1dol <tmd.id2004@gmail.com>
2026-05-22 15:56:41 +0700

    feat: simplify parser

--- internal/adapter/jmdict/parser.go
+++ internal/adapter/jmdict/parser.go
@@ -3,101 +3,25 @@ package jmdict
 import (
 	"encoding/json"
 	"io"
-	"strings"
 
 	"github.com/Tranduy1dol/kotoba-press-core/internal/domain"
 )
 
-type JMdictFile struct {
-	Words []JMdictWord `json:"words"`
-}
-
-type JMdictWord struct {
-	ID    string        `json:"id"`
-	Kanji []JMdictKanji `json:"kanji"`
-	Kana  []JMdictKana  `json:"kana"`
-	Sense []JMdictSense `json:"sense"`
-}
-
-type JMdictKanji struct {
-	Common bool     `json:"common"`
-	Text   string   `json:"text"`
-	Tags   []string `json:"tags"`
-}
-
-type JMdictKana struct {
-	Common         bool     `json:"common"`
-	Text           string   `json:"text"`
-	Tags           []string `json:"tags"`
-	AppliesToKanji []string `json:"appliesToKanji"`
-}
-
-type JMdictSense struct {
-	PartOfSpeech []string      `json:"partOfSpeech"`
-	Gloss        []JMdictGloss `json:"gloss"`
-	Field        []string      `json:"field"`
-	Dialect      []string      `json:"dialect"`
-}
-
-type JMdictGloss struct {
-	Lang string `json:"lang"`
-	Text string `json:"text"`
+type DictFile struct {
+	Words []domain.Word `json:"words"`
 }
 
 func Parse(reader io.Reader) ([]*domain.Word, error) {
-	var file JMdictFile
-	err := json.NewDecoder(reader).Decode(&file)
-	if err != nil {
+	var file DictFile
+	if err := json.NewDecoder(reader).Decode(&file); err != nil {
 		return nil, err
 	}
 
 	words := make([]*domain.Word, 0, len(file.Words))
-	for _, e := range file.Words {
-		word := new(domain.Word)
-
-		word.ID = e.ID
-		word.EntSeq = e.ID
-		word.Source = "jmdict"
-
-		for _, k := range e.Kanji {
-			word.Kanji = append(word.Kanji, domain.Kanji{
-				Text: k.Text,
-				Info: strings.Join(k.Tags, ","),
-			})
-
-			if k.Common {
-				word.IsCommon = true
-			}
-		}
-
-		for _, r := range e.Kana {
-			word.Readings = append(word.Readings, domain.Reading{
-				Text: r.Text,
-				Info: r.Tags,
-			})
-
-			if r.Common {
-				word.IsCommon = true
-			}
-		}
-
-		for _, s := range e.Sense {
-			sense := domain.Sense{
-				POS:    s.PartOfSpeech,
-				Source: strings.Join(s.Field, ","),
-			}
-
-			for _, g := range s.Gloss {
-				sense.Gloss = append(sense.Gloss, domain.Gloss{
-					Text: g.Text,
-					Lang: g.Lang,
-				})
-			}
-
-			word.Senses = append(word.Senses, sense)
-		}
-
-		words = append(words, word)
+	for i := range file.Words {
+		w := &file.Words[i]
+		w.Source = "admin"
+		words = append(words, w)
 	}
 
 	return words, nil
--- internal/domain/word.go
+++ internal/domain/word.go
@@ -3,39 +3,39 @@ package domain
 import "strings"
 
 type Word struct {
-	ID        string    `bson:"_id"`
-	EntSeq    string    `bson:"ent_seq"`
-	Kanji     []Kanji   `bson:"kanji"`
-	Readings  []Reading `bson:"readings"`
-	Senses    []Sense   `bson:"senses"`
-	JLPT      int       `bson:"jlpt"`
-	IsCommon  bool      `bson:"is_common"`
-	Source    string    `bson:"source"`
-	CreatedBy string    `bson:"created_by"`
+	ID        string    `bson:"_id" json:"id"`
+	EntSeq    string    `bson:"ent_seq" json:"ent_seq,omitempty"`
+	Kanji     []Kanji   `bson:"kanji" json:"kanji"`
+	Readings  []Reading `bson:"readings" json:"readings"`
+	Senses    []Sense   `bson:"senses" json:"senses"`
+	JLPT      int       `bson:"jlpt" json:"jlpt,omitempty"`
+	IsCommon  bool      `bson:"is_common" json:"is_common,omitempty"`
+	Source    string    `bson:"source" json:"source,omitempty"`
+	CreatedBy string    `bson:"created_by" json:"created_by,omitempty"`
 }
 
 type Kanji struct {
-	Text     string `bson:"text"`
-	Info     string `bson:"info"`
-	Priority int    `bson:"priority"`
+	Text     string `bson:"text" json:"text"`
+	Info     string `bson:"info" json:"info,omitempty"`
+	Priority int    `bson:"priority" json:"priority,omitempty"`
 }
 
 type Reading struct {
-	Text     string   `bson:"text"`
-	Status   string   `bson:"status"`
-	Info     []string `bson:"info"`
-	Priority int      `bson:"priority"`
+	Text     string   `bson:"text" json:"text"`
+	Status   string   `bson:"status" json:"status,omitempty"`
+	Info     []string `bson:"info" json:"info,omitempty"`
+	Priority int      `bson:"priority" json:"priority,omitempty"`
 }
 
 type Sense struct {
-	POS    []string `bson:"pos"`
-	Gloss  []Gloss  `bson:"gloss"`
-	Source string   `bson:"source"`
+	POS    []string `bson:"pos" json:"pos"`
+	Gloss  []Gloss  `bson:"gloss" json:"gloss"`
+	Source string   `bson:"source" json:"source,omitempty"`
 }
 
 type Gloss struct {
-	Text string `bson:"text"`
-	Lang string `bson:"lang"`
+	Text string `bson:"text" json:"text"`
+	Lang string `bson:"lang" json:"lang"`
 }
 
 func (w *Word) SearchText() (title string, text string) {
